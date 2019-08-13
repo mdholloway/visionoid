01e8e5817bc55fbb81260d5444dcbd5e6146e64e
Michael Holloway <mholloway@wikimedia.org>
2019-08-13 13:29:04 -0400

    Abstractify providers
    Adds support for specifying the provider in a query param. Sets up a
    bare-bones provider interface that can be implemented by adding a file
    in lib/providers and implementing fetchTags.

--- routes/tags.js
+++ routes/tags.js
@@ -1,27 +1,16 @@
 'use strict';
 
-const _ = require('lodash');
-const words = require('fs').readFileSync(require('word-list'), 'utf8').split('\n');
 const router = require('../lib/util').router();
-
-function fetchTags(imageTitle, imageUrl) {
-    const payload = {
-        title: imageTitle,
-        timestamp: Date.now(),
-        provider: 'random',
-        labels: _.sampleSize(words, 10).map((label) => ({ wikidata_id: `Q${_.random(0, 1000000)}`, label }))
-    };
-    const response = { headers: {}, body: payload };
-    return Promise.resolve(response);
-}
 
 router.get('/labels', (req, res) => {
     const imageTitle = req.query.title;
     const imageUrl = req.query.url;
-    fetchTags(imageTitle, imageUrl).then((tagResponse) => {
-        const body = tagResponse.body;
-        res.json(body);
-    });
+    const provider = req.query.provider || 'random';
+    require(`../lib/providers/${provider}`).fetchTags(imageTitle, imageUrl)
+        .then((tagResponse) => {
+            const body = tagResponse.body;
+            res.json(body);
+        });
 });
 
 module.exports = () => {
