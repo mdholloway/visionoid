9dc558b9451bfc3fa0a39a783287d2ee6fbd93d1
Michael Holloway <mholloway@wikimedia.org>
2019-08-15 20:58:29 -0400

    Allow targeting different Wikidatas
    Allow targeting common-item suggestions to beta, prod, or local (default
    prod) using a 'target' query parameter.
    
    Also stop sending predefined label strings since these will be derived
    from local mappings.

--- routes/tags.js
+++ routes/tags.js
@@ -6,7 +6,8 @@ router.get('/labels', (req, res) => {
     const imageTitle = req.query.title;
     const imageUrl = req.query.url;
     const provider = req.query.provider || 'common';
-    require(`../lib/providers/${provider}`).fetchTags(imageTitle, imageUrl)
+    const target =  req.query.target;
+    require(`../lib/providers/${provider}`).fetchTags(imageTitle, imageUrl, target)
         .then((tagResponse) => {
             const body = tagResponse.body;
             res.json(body);
